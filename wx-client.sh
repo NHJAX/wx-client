cbca9f1de97bd7ffbe42160aa35c08d020f30127
Alex Rangeo <arangeo@conceptplusllc.com>
2019-04-19 07:48:10 -0400

    updating output

--- wx-client.sh
+++ wx-client.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /home/pi/apps/wx-client
-echo pwd
 git pull
 echo "just did git pull"
 npm install
 echo "just ran npm install"
 pm2 startOrRestart ecosystem.config.js
-echo pm2 ls
+echo "just started or restarted ecosystem"
