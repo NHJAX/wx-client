1a317cf46a54c71f6fe816d5ff37a9c150950fb2
Alex Rangeo <arangeo@conceptplusllc.com>
2019-04-19 07:51:35 -0400

    add path to commands

--- wx-client.sh
+++ wx-client.sh
@@ -2,7 +2,7 @@
 cd /home/pi/apps/wx-client
 git pull
 echo "just did git pull"
-npm install
+/usr/local/bin/npm install
 echo "just ran npm install"
-pm2 startOrRestart ecosystem.config.js
+/usr/local/bin/pm2 startOrRestart ecosystem.config.js
 echo "just started or restarted ecosystem"
