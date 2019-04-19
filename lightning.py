49402c7cfea7746a04994f76a17ff6e73c088054
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-19 11:20:30 -0400

    no message

--- lightning.py
+++ lightning.py
@@ -8,7 +8,8 @@ from datetime import datetime
 now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
 with open('/home/pi/apps/secret-config/api-config.json') as json_data_file:
     data = json.load(json_data_file)
-public_key = resource_path('/home/pi/apps/secret-config/certs/publickey.pem')
+with open('/home/pi/apps/secret-config/certs/publickey.pem') as pk:
+    public_key = resource_path(pk)
 URL = data['BASEURL']
 HEADERS = {'NHJax-API-Key':data['NHJax-API-Key']}
 Topic = data['location']
