d828f1988a06354dc961501d053aa04ae7103344
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-22 07:03:22 -0400

    fixed there code

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ try:
 
        sensor.set_indoors(False)
 
-       print "Thunder Board present at address 0x02"
+       print ("Thunder Board present at address 0x02")
 
 except IOError as e:
 	sensor = AS3935(address=0x03, bus=1)
@@ -26,11 +26,11 @@ except IOError as e:
 
                	sensor.set_indoors(False)
 
-               	print "Thunder Board present at address 0x03"
+               	print ("Thunder Board present at address 0x03")
 
        	except IOError as e:
 
-        	print "Thunder Board not present"
+        	print ("Thunder Board not present")
 		exit()
 
 sensor.set_indoors(False)
@@ -46,26 +46,26 @@ def handle_interrupt(channel):
     time.sleep(0.003)
     global sensor
     reason = sensor.get_interrupt()
-    print "Interrupt reason=", reason
+    print ("Interrupt reason=", reason)
     if reason == 0x01:
-        print "Noise level too high - adjusting"
+        print ("Noise level too high - adjusting")
         sensor.raise_noise_floor()
     elif reason == 0x04:
-        print "Disturber detected - masking"
+        print ("Disturber detected - masking")
         sensor.set_mask_disturber(True)
     elif reason == 0x08:
         now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
         distance = sensor.get_distance()
-        print "We sensed lightning!"
-        print "It was " + str(distance) + "km away. (%s)" % now
-        print ""
+        print ("We sensed lightning!")
+        print ("It was " + str(distance) + "km away. (%s)" % now)
+        print ("")
 
 
 #GPIO.setup(InterruptGPIOpin, GPIO.IN )
 GPIO.setup(InterruptGPIOpin, GPIO.IN, pull_up_down = GPIO.PUD_UP )
 GPIO.add_event_detect(InterruptGPIOpin, GPIO.RISING, callback=handle_interrupt)
 
-print "Waiting for lightning - or at least something that looks like it"
+print ("Waiting for lightning - or at least something that looks like it")
 
 
 def readLightningStatus():
@@ -78,14 +78,14 @@ def readLightningStatus():
 	disp_lco = sensor.get_disp_lco()
 	#interrupt = sensor.get_interrupt()
 
-	print "---------"
-	print "distance=", distance
-	print "noise_floor=", noise_floor
-	print "min_strikes=", min_strikes
-	print "indoor=", indoor
-	print "mask_disturber=", mask_disturber
-	print "disp_lco=", disp_lco
-	print "count=", count
+	print ("---------")
+	print ("distance=", distance)
+	print ("noise_floor=", noise_floor)
+	print ("min_strikes=", min_strikes)
+	print ("indoor=", indoor)
+	print ("mask_disturber=", mask_disturber)
+	print ("disp_lco=", disp_lco)
+	print ("count=", count)
 	#print "interrupt=", interrupt
 
 
