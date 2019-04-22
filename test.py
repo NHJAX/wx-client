a7b23157e5f2c5f726dfb4f01e0f503609351c6e
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-22 13:19:53 -0400

    removed old messaging

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import smtplib
 import socket
 from email.mime.text import MIMEText
 import datetime
-to = 'nhjax-dev@'
+to = 'nhjax-dev@gmail.com'
 gmail_user = 'rpiwxbox@gmail.com'
 gmail_password = 'ZAQ!2wsx'
 smtpserver = smtplib.SMTP('smtp.gmail.com',587)
@@ -12,15 +12,7 @@ smtpserver.starttls()
 smtpserver.ehlo
 smtpserver.login(gmail_user, gmail_password)
 today = datetime.date.today()
-brg='iwgetid -r'
-s=subprocess.Popen(brg,shell=True,stdout=subprocess.PIPE)
-ssid = s.communicate()
-arg='ip route list'
-p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
-data = p.communicate()
-split_data = data[0].split()
-ipaddr = split_data[split_data.index('src')+1]   
-my_ssid = "NMRTC JAX RPI is connected to '{0}' IP address is '{1}'" .format(ssid, ipaddr)
+my_ssid = "NMRTC JAX RPI is connected"
 msg = MIMEText(my_ssid)
 msg['Subject'] = 'IP For NMRTC JAX RaspberryPi on %s' % today.strftime('%b %d %Y')
 msg['From'] = gmail_user
