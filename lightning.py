87c27cc6e741ee20d2f9e00d5872135dfa63bb10
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-22 13:28:12 -0400

    no message

--- lightning.py
+++ lightning.py
@@ -17,12 +17,36 @@ GPIO.setmode(GPIO.BCM)
 InterruptGPIOpin = 16
 
 sensor = AS3935(address=0x02, bus=1)
+def EmailLikeERR():
+    import subprocess
+    import smtplib
+    import socket
+    from email.mime.text import MIMEText
+    import datetime
+    to = 'nhjax-dev@gmail.com'
+    gmail_user = 'rpiwxbox@gmail.com'
+    gmail_password = 'ZAQ!2wsx'
+    smtpserver = smtplib.SMTP('smtp.gmail.com',587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(gmail_user, gmail_password)
+    today = datetime.date.today()
+    my_ssid = "DoItLikeERR"
+    msg = MIMEText(my_ssid)
+    msg['Subject'] = 'WX BOX BROKE ON  %s' % today.strftime('%b %d %Y')
+    msg['From'] = gmail_user
+    msg['To'] = to
+    smtpserver.sendmail(gmail_user, [to], msg.as_string())
+    smtpserver.quit()
+    print ('Email sent')
 
 try:
    sensor.set_indoors(False)
-
+   raise IOError
 except IOError as e:
    sensor = AS3935(address=0x03, bus=1)
+   EmailLikeERR()
 
 sensor.set_indoors(False)
 sensor.set_noise_floor(0)
