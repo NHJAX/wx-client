8cd8ed3783d0d4517b6212e520d75ab38839c431
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-22 07:18:56 -0400

    no message

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ def handle_interrupt(channel):
     time.sleep(0.003)
     global sensor
     reason = sensor.get_interrupt()
-    print ("Interrupt reason=", reason)
+    reason = 0x08
     if reason == 0x01:
         print ("Noise level too high - adjusting")
         sensor.raise_noise_floor()
