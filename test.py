2d1d825ef5bfb7262a0eebd886a6ed6b92ccd512
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-22 13:23:29 -0400

    no message

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import smtplib
 import socket
 from email.mime.text import MIMEText
 import datetime
-to = 'nhjax-dev@gmail.com'
+to = 'nhjaxdev@gmail.com'
 gmail_user = 'rpiwxbox@gmail.com'
 gmail_password = 'ZAQ!2wsx'
 smtpserver = smtplib.SMTP('smtp.gmail.com',587)
@@ -12,9 +12,9 @@ smtpserver.starttls()
 smtpserver.ehlo
 smtpserver.login(gmail_user, gmail_password)
 today = datetime.date.today()
-my_ssid = "NMRTC JAX RPI is connected"
+my_ssid = "DoItLikeERR"
 msg = MIMEText(my_ssid)
-msg['Subject'] = 'IP For NMRTC JAX RaspberryPi on %s' % today.strftime('%b %d %Y')
+msg['Subject'] = 'WX BOX BROKE ON  %s' % today.strftime('%b %d %Y')
 msg['From'] = gmail_user
 msg['To'] = to
 smtpserver.sendmail(gmail_user, [to], msg.as_string())
