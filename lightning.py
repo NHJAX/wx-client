8d09afa8f3e65baa62fab78430948b967b1031bc
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-22 08:45:33 -0400

    moved new code over

--- lightning.py
+++ lightning.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 from SDL_Pi_Thunderboard_AS3935 import  AS3935
 import json
+import requests
 import RPi.GPIO as GPIO
 import time
-import requests
-import sys
-import os
 from datetime import datetime
-# now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
-# with open('/home/pi/apps/secret-config/api-config.json') as json_data_file:
-#     data = json.load(json_data_file)
-# public_key = ('/home/pi/apps/secret-config/certs/publickey.pem')
-# URL = data['BASEURL']
-# HEADERS = {'NHJax-API-Key':data['NHJax-API-Key']}
-# Topic = data['location']
-
-#requests.post(url= URL, headers= HEADERS, data= payload)
-
-#print (URL)
-#print (HEADERS)
-#print (Topic)
+now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
+with open('/home/pi/apps/secret-config/api-config.json') as json_data_file:
+    data = json.load(json_data_file)
+URL = data['BASEURL']
+HEADERS = {'NHJax-API-Key':data['NHJax-API-Key']}
+Topic = data['LOCATION']
 
 GPIO.setmode(GPIO.BCM)
 
@@ -27,87 +18,47 @@ InterruptGPIOpin = 16
 
 sensor = AS3935(address=0x02, bus=1)
 
-sensor.reset()
-
-
 try:
-
-    sensor.set_indoors(True)
-
-    print ("Thunder Board present at address 0x02")
+   sensor.set_indoors(False)
 
 except IOError as e:
+   sensor = AS3935(address=0x03, bus=1)
 
-    sensor = AS3935(address=0x03, bus=1)
-
-    try:
-
-        sensor.set_indoors(True)
-
-        print ("Thunder Board present at address 0x03")
-
-    except IOError as e:
-        print ("Thunder Board not present")
-        exit()
-
-sensor.set_indoors(True)
+sensor.set_indoors(False)
 sensor.set_noise_floor(0)
-sensor.calibrate(tun_cap=None)
+sensor.calibrate(tun_cap=0x09)
 sensor.set_min_strikes(1)
 
 count = 0
 runcount = 0
 def handle_interrupt(channel):
     global count
-    global test
     count = count + 1
     time.sleep(0.003)
     global sensor
     reason = sensor.get_interrupt()
     if reason == 0x01:
-        print ("Noise")
         sensor.raise_noise_floor()
     elif reason == 0x04:
-        print ("Disturber")
         sensor.set_mask_disturber(True)
-    elif reason == 0x08:
+    else:
         now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
         distance = sensor.get_distance()
         payload = {"Type": "Lighting", "LightningDetected": "Yes", "Location": Topic, "DistanceKM": distance, "Time": now}
         print (payload)
-        # requests.post(url= URL, headers= HEADERS, data= payload, verify= public_key)
+        requests.post(url= URL, headers= HEADERS, data= payload)
 
-
-#GPIO.setup(InterruptGPIOpin, GPIO.IN )
 GPIO.setup(InterruptGPIOpin, GPIO.IN, pull_up_down = GPIO.PUD_UP )
 GPIO.add_event_detect(InterruptGPIOpin, GPIO.RISING, callback=handle_interrupt)
 
-print ("Waiting for lightning - or at least something that looks like it")
-# payload = {"Type": "Lighting", "LightningDetected": "Yes", "Location": Topic, "DistanceKM": "0", "Time": "0"}
-# requests.post(url= URL, headers= HEADERS, data= payload, verify= public_key)
+def readLightningStatus():
 
-
-# def readLightningStatus():
-
-	# distance = sensor.get_distance()
-	# noise_floor = sensor.get_noise_floor()
-	# min_strikes = sensor.get_min_strikes()
-	# indoor = sensor.get_indoors()
-	# mask_disturber = sensor.get_mask_disturber()
-	# disp_lco = sensor.get_disp_lco()
-	# #interrupt = sensor.get_interrupt()
-    #
-	# print "---------"
-	# print "distance=", distance
-	# print "noise_floor=", noise_floor
-	# print "min_strikes=", min_strikes
-	# print "indoor=", indoor
-	# print "mask_disturber=", mask_disturber
-	# print "disp_lco=", disp_lco
-	# print "count=", count
-	# #print "interrupt=", interrupt
-
+	distance = sensor.get_distance()
+	noise_floor = sensor.get_noise_floor()
+	min_strikes = sensor.get_min_strikes()
+	indoor = sensor.get_indoors()
+	mask_disturber = sensor.get_mask_disturber()
+	disp_lco = sensor.get_disp_lco()
 
 while True:
     time.sleep(1.0)
-    #readLightningStatus()
