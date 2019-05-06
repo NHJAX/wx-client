ec9ac92ab2df1f4a32573aee0f6ab5fd106b6fea
Kevin nichols <kevinnichols1321@icloud.com>
2019-05-06 07:40:15 -0400

    degraded lightning

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,21 +13,6 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
-  },
-  {
-    name: 'LightningDetector',
-    script: 'python lightning.py',
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
   }
   ]
 };
