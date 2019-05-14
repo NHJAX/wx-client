84215e03be1dbb0c93516dca503e154f2d0fdede
Kevin nichols <kevinnichols1321@icloud.com>
2019-05-14 10:02:02 -0400

    testing dual dht11 sensor setup

---
+++ test.js
@@ -0,0 +1,67 @@
+var sensor = require('node-dht-sensor'); //little blue sensor modual
+
+var retries = 0;
+var tempright = 0;
+var templeft = 0;
+var humright = 0;
+var humleft = 0;
+var objToSendright = "";
+var objToSendleft = "";
+
+function WX() { //fires every 15 minutes
+
+    sensor.read(11, 4, function(err, tempright, humright) {
+        if (!err) {
+            var objToSendright = {
+                "location": API_CONFIG["LOCATION"],
+                "temp": (tempright.toFixed(1)),
+                "hum": (humright.toFixed(1))
+            }
+            console.log('Sending: ', objToSendright);
+            //calling the MQTT function
+            //terminator(objToSend);
+            return;
+        } else {
+            console.log("there was an error", err);
+            if (retries <= 2){
+                retries++;
+                WX(); //refire
+            } else {
+                retries = 0;
+                return;
+            }
+
+        }
+    });
+
+    sensor.read(11, 17, function(err, templeft, humleft) {
+        if (!err) {
+            var objToSendleft = {
+                "location": API_CONFIG["LOCATION"],
+                "temp": (templeft.toFixed(1)),
+                "hum": (humleft.toFixed(1))
+            }
+            console.log('Sending: ', objToSendleft);
+            //calling the MQTT function
+            //terminator(objToSend);
+            return;
+        } else {
+            console.log("there was an error", err);
+            if (retries <= 2){
+                retries++;
+                WX(); //refire
+            } else {
+                retries = 0;
+                return;
+            }
+
+        }
+    });
+    console.log(objToSendleft);
+    console.log(objToSendright);
+
+//}, null, true, 'America/New_York');
+
+};
+WX();
+//setInterval(WX, 10000); //loops WX function every 10 seconds (10000 milliseconds) TO INFINITY AND BEYOND OR ATLEAST UNTIL A REBOOT
