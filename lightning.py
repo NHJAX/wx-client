2b821f243ba851e91d3474daa742e5106bf4be77
Kevin nichols <kevinnichols1321@icloud.com>
2019-05-28 06:52:44 -0400

    We are gonna try for detected lightning again with a new board

--- lightning.py
+++ lightning.py
@@ -1,103 +1,90 @@
 #!/usr/bin/env python
-from SDL_Pi_Thunderboard_AS3935 import  AS3935
-import json
-import requests
+# -*- coding: utf-8 -*-
+from RPi_AS3935 import RPi_AS3935
 import RPi.GPIO as GPIO
+import thread
 import time
 from datetime import datetime
-now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
-with open('/home/pi/apps/secret-config/api-config.json') as json_data_file:
-    data = json.load(json_data_file)
-URL = data['BASEURL']
-HEADERS = {'NHJax-API-Key':data['NHJax-API-Key']}
-Topic = data['LOCATION']
-tap = data['Gmail']
 
+# Initialize GPIO
 GPIO.setmode(GPIO.BCM)
 
-InterruptGPIOpin = 16
-
-sensor = AS3935(address=0x02, bus=1)
-def EmailLikeERR():
-    print ('You got a error like ERRR')
-    import subprocess
-    import smtplib
-    import socket
-    from email.mime.text import MIMEText
-    import datetime
-    to = 'nhjax-dev@gmail.com'
-    gmail_user = 'rpiwxbox@gmail.com'
-    smtpserver = smtplib.SMTP('smtp.gmail.com',587)
-    smtpserver.ehlo()
-    smtpserver.starttls()
-    smtpserver.ehlo
-    smtpserver.login(gmail_user, tap)
-    today = datetime.date.today()
-    my_ssid = "DoItLikeERR"
-    msg = MIMEText(my_ssid)
-    msg['Subject'] = 'WX BOX BROKE ON  %s' % today.strftime('%b %d %Y')
-    msg['From'] = gmail_user
-    msg['To'] = to
-    smtpserver.sendmail(gmail_user, [to], msg.as_string())
-    smtpserver.quit()
-    print ('Email sent')
-
-def SendPayload():
-    global HEADERS
-    global URL
-    global payload
-    try:
-        print ("Sending That Message")
-        requests.post(url= URL, headers= HEADERS, data= payload)
-    except Exception as e:
-        print ("CANNOT SEE THE SERVER!!! the error is ", e)
-
-try:
-   sensor.set_indoors(False)
-   print ('Sensor Online')
-   #raise IOError #uncomment to test err handler
-except IOError as e:
-   sensor = AS3935(address=0x03, bus=1)
-   EmailLikeERR()
-
+# pin used for interrupts
+pin = 18
+# Rev. 1 Raspberry Pis should leave bus set at 0, while rev. 2 Pis should set
+# bus equal to 1. The address should be changed to match the address of the
+# sensor.
+sensor = RPi_AS3935(address=0x03, bus=1)
+# Indoors = more sensitive (can miss very strong lightnings)
+# Outdoors = less sensitive (can miss far away lightnings)
 sensor.set_indoors(False)
-sensor.set_noise_floor(0)
-sensor.calibrate(tun_cap=0x09)
-sensor.set_min_strikes(1)
+sensor.set_noise_floor(1)
+# Change this value to the tuning value for your sensor
+sensor.calibrate(tun_cap=0x01)
+# Prevent single isolated strikes from being logged => interrupts begin after 5 strikes, then are fired normally
+sensor.set_min_strikes(5)
 
-count = 0
-runcount = 0
+last_alert = datetime.min
+strikes_since_last_alert = 0
+
+
+# interrupt handle
+
+
+
+# Interrupt handler
 def handle_interrupt(channel):
-    global count
-    global payload
-    count = count + 1
-    time.sleep(0.003)
+    global last_alert
+    global strikes_since_last_alert
     global sensor
+    current_timestamp = datetime.now()
+    time.sleep(0.003)
     reason = sensor.get_interrupt()
     if reason == 0x01:
+        print("Noise level too high - adjusting")
         sensor.raise_noise_floor()
     elif reason == 0x04:
+        print("Disturber detected. Masking subsequent disturbers")
         sensor.set_mask_disturber(True)
-    else:
-        print ("Whoa did you see that")
-        now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
+    elif reason == 0x08:
+        print("We sensed lightning! (%s)" % current_timestamp.strftime('%H:%M:%S - %Y/%m/%d'))
+        if (current_timestamp - last_alert).seconds < 300:
+            print("Last strike is too recent, incrementing counter since last alert.")
+            strikes_since_last_alert += 1
+            return
         distance = sensor.get_distance()
-        payload = {"Type": "Lighting", "LightningDetected": "Yes", "Location": Topic, "DistanceKM": distance, "Time": now}
-        print (payload)
-        SendPayload()
+        energy = sensor.get_energy()
+        print("Energy: " + str(energy) + " - distance: " + str(distance) + "km")
+        # Yes, it tweets in French. Baguette.
+        # if strikes_since_last_alert == 0:
+        #     thread.start_new_thread(send_tweet, (
+        #         "Eclair détecté ! Puissance : {0} - distance du front de tempête : {1}km".format(energy, distance),))
+        # else:
+        #     thread.start_new_thread(send_tweet, (
+        #         "/!\ {2} éclairs détectés ces {3} dernières minutes. Puissance dernier éclair : {0} - distance du front de tempête : {1}km".format(
+        #             energy, distance, strikes_since_last_alert + 1, (current_timestamp - last_alert).seconds / 60),))
+        strikes_since_last_alert = 0
+        last_alert = current_timestamp
+    # If no strike has been detected for the last hour, reset the strikes_since_last_alert (consider storm finished)
+    # if (current_timestamp - last_alert).seconds > 1800 and last_alert != datetime.min:
+    #     thread.start_new_thread(send_tweet, (
+    #             "\o/ Orage terminé. Aucun nouvel éclair détecté depuis 1/2h.",))
+    #     strikes_since_last_alert = 0
+    #     last_alert = datetime.min
 
-GPIO.setup(InterruptGPIOpin, GPIO.IN, pull_up_down = GPIO.PUD_UP )
-GPIO.add_event_detect(InterruptGPIOpin, GPIO.RISING, callback=handle_interrupt)
 
-print ("Lightning Detection Online @ ", now)
-def readLightningStatus():
+# Use a software Pull-Down on interrupt pin
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+sensor.set_mask_disturber(False)
+GPIO.add_event_detect(pin, GPIO.RISING, callback=handle_interrupt)
 
-	distance = sensor.get_distance()
-	noise_floor = sensor.get_noise_floor()
-	min_strikes = sensor.get_min_strikes()
-	indoor = sensor.get_indoors()
-	mask_disturber = sensor.get_mask_disturber()
-	disp_lco = sensor.get_disp_lco()
+print("Waiting for lightning - or at least something that looks like it")
 
-while True:
-    time.sleep(1.0)
+try:
+    while True:
+        # Read/clear the sensor data every 10s in case we missed an interrupt (interrupts happening too fast ?)
+        time.sleep(10)
+        handle_interrupt(pin)
+finally:
+    # cleanup used pins... just because we like cleaning up after us
+    GPIO.cleanup()
